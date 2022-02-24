c4a86bc306cc243729f5ecfe4479b4f8d7caa416
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-02-24 16:51:36 +0100

    add show style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 
 @section('content')
-    <ul>
+    {{-- <ul>
         <li>{{ $comic->title }}</li>
         <li>{{ $comic->original_title }}</li>
         <li>{{ $comic->volume_n }}</li>
@@ -11,5 +11,24 @@
         <li>{{ $comic->price }}</li>
         <li><a href="{{ route('comics.index') }}">Return</a></li>
         <li><a href="{{ route('comics.edit', $comic->id) }}">Edit</a></li>
-    </ul>
+    </ul> --}}
+    
+    <div class="row">
+        <div class="col">
+            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $comic->title }}</h5>
+                    <h5 class="card-title">{{ $comic->original_title }}</h5>
+                    <img src="{{ $comic->photo }}" class="card-img-top" alt="...">
+                    <h5 class="card-title">{{ $comic->volume_n }}</h5>
+                    <h5 class="card-title">{{ $comic->editor }}</h5>
+                    <h5 class="card-title">{{ $comic->author }}</h5>
+                    <h5 class="card-title">{{ $comic->price }}</h5>
+                    <a class="card-title text-decoration-none text-white btn btn-danger" href="{{ route('comics.index') }}">Return</a>
+                    <a class="card-title text-decoration-none text-dark btn btn-warning" href="{{ route('comics.edit', $comic->id) }}">Edit</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
